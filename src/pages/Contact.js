82fc72c62858748cbcfd43726b642d1d976a18ae
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-09-06 16:12:02 -0500

    update-form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,13 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className="contact">
-      contact page
+      <form>
+        <label>name</label>
+        <input type="text" placeholder=" type your Name"/>
+        <label>email</label>
+       <input type="email" placeholder="email"/>
+
+      </form>
     </div>
   )
 }
